16e80012c428b9761ab1f38d449b57083413b06c
宋晓奎 <austsxk@163.com>
2021-01-29 12:46:24 +0800

    # 30-1 使用test table driven进行批量数据测试

--- Testing/1.maxSubString_test.go
+++ Testing/1.maxSubString_test.go
@@ -1,7 +1,6 @@
 package Testing
 
 import (
-	"fmt"
 	"testing"
 )
 
@@ -14,10 +13,19 @@ import (
 */
 
 func TestLengthOfLongestSubStringBitMap(t *testing.T) {
-	var x = "abcabcbbytrsdf"
-	start, end, max, st := lengthOfLongestSubStringBitMap(x)
-	if st != "bytrsdf" {
-		t.Fail()
+	var testCase = []struct {
+		s      string
+		target string
+	}{
+		{"abcabcbbytrsdf", "bytrsdf"},
+		{"pwwasd", "wasd"},
+		{"asdfggggg", "asdfg"},
+		{"ggggg", "g"},
 	}
-	fmt.Println(start, end, max)
+	for _, v := range testCase {
+		_, _, _, st := lengthOfLongestSubStringBitMap(v.s)
+		if st != v.target {
+			t.Errorf("ErrInfo: %s的值应该是:%s,不是：%s", v.s, v.target, st)
+		}
+	}
 }
